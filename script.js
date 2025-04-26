288c8ed0fe40b73ee24b17df10dd8df03decfb6d
Ranbeer Malhotra <ranbeermalhotra2004@gmail.com>
2025-04-26 19:53:53 +0530

    feat(buttons): addEventListenershow operation on display

--- script.js
+++ script.js
@@ -29,3 +29,15 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2;
 }
+
+// Event Listeners
+disp = document.querySelector('.calc-display');
+btns = Array.from(document.querySelectorAll('.math'));
+
+// click event for btns
+btns.forEach((element) => {
+  element.addEventListener('click', (e) => {
+    let calc = document.createTextNode(e.target.textContent);
+    disp.appendChild(calc);
+  })
+});
