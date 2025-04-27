1db3e54d3da4947922fa1791e2c70d88851dce44
Ranbeer Malhotra <ranbeermalhotra2004@gmail.com>
2025-04-27 08:04:27 +0530

    feat(buttons): addEventListener show mathematical operators and save the operator

--- script.js
+++ script.js
@@ -33,11 +33,19 @@ function divide(num1, num2) {
 // Event Listeners
 const disp = document.querySelector('.calc-display');
 const numbers = Array.from(document.querySelectorAll('.number'));
+const operations = Array.from(document.querySelectorAll('.op'));
 // click event for number
 numbers.forEach(element => {
   element.addEventListener('click', (e) => {
     let calc = document.createTextNode(e.target.textContent);
     disp.appendChild(calc);
+  });
+});
+// click event for operations
+operations.forEach(element => {
+  element.addEventListener('click', (e) => {
+    op = e.target.textContent;
+    disp.appendChild(document.createTextNode(op));
   });
 });
 });
